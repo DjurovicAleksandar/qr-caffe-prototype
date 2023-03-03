b4e26dae4e080d3a402704911fc5fd7df05b33d4
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-03-03 19:58:58 +0100

    change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,17 @@
-import { BsFillCartFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { BsFillCartFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
     <div className="bg-transparent text-green-200 p-4 text-2xl flex items-center justify-between">
       <Link to="/">
-        {" "}
-        <h1 className="cursor-pointer">QRbar</h1>{" "}
+        {' '}
+        <h1 className="cursor-pointer">QRbar</h1>{' '}
       </Link>
       <button className="hover:scale-90">
         <BsFillCartFill />
       </button>
+      <Link to="/caffe-login">Login</Link>
     </div>
   );
 }
